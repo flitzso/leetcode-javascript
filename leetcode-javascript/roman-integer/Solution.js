eaf50bd82b0a5922805460170b9ca0398ac4010f
Flitzso <106411702+flitzso@users.noreply.github.com>
2023-08-10 13:22:50 -0300

    Add files via upload

---
+++ leetcode-javascript/roman-integer/Solution.js
@@ -0,0 +1,36 @@
+/**Como executar Terminal: node Solution.js
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+        const romanValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+    
+    let result = 0;
+    let prevValue = 0;
+    
+    for (let i = s.length - 1; i >= 0; i--) {
+        const currentValue = romanValues[s.charAt(i)];
+        if (currentValue >= prevValue) {
+            result += currentValue;
+        } else {
+            result -= currentValue;
+        }
+        prevValue = currentValue;
+    }
+    
+    return result;
+
+
+console.log(romanToInt("III"));
+console.log(romanToInt("LVIII"));
+console.log(romanToInt("MCMXCIV"));
+
+};
